17fa17ed9ab6c84ebe86b10897cee95eb9d9a9e5
hansufsm <hans@ufsm.br>
2026-07-09 15:26:55 -0300

    security: adiciona tags robots noindex nofollow na rota de extrato publico

---
+++ app/[cpfPrefix]/layout.tsx
@@ -0,0 +1,22 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true,
+    },
+  },
+};
+
+export default function PublicExtratoLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
